032da4e94de758e34adb538a80f36cabc4255ef1
Majdag8 <majdag2005@gmail.com>
2024-04-06 14:23:50 +0300

    add to cart

--- addToCart.php
+++ addToCart.php
@@ -2,40 +2,33 @@
 session_start();
 include "config.php";
 
-if (isset($_POST['product_id']) && isset($_POST['quantity'])) {
+if (isset($_POST['product_id'])) {
     $product_id = $_POST['product_id'];
-    $quantity = $_POST['quantity'];
-    $_SESSION['cart'][$product_id] = $quantity;
-    $cartHtml = generateCartHtml($_SESSION['cart'], $conn); // Pass $conn as an argument
+    $_SESSION['id'] = $product_id;
+    $cartHtml = generateCartHtml($_SESSION['id'], $conn);
     echo $cartHtml;
 } else {
-    echo 'Error: Missing product ID or quantity.';
+    echo 'Error: Missing product.';
 }
 
-// Function to generate cart HTML
-function generateCartHtml($cart, $conn) { // Add $conn as a parameter
+function generateCartHtml($product_id, $conn) {
     $html = '';
-    foreach ($cart as $product_id => $quantity) {
-        // Fetch product details from the database
-        $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
-        $stmt->bind_param("i", $product_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
+    $stmt = $conn->prepare("SELECT * FROM products WHERE product_id = ?");
+    $stmt->bind_param("i", $product_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        // Display the product details in the cart item
-        if ($product = $result->fetch_assoc()) { // Fetch the product details
-            $html .= '<div class="item">
-                            <div class="image">
-                                <img src="shoes_imgs/'.$product['imageURL'].'">
-                            </div>
-                            <div class="name">' . $product['product_name'] . '</div>
-                            <div class="totalPrice">' . $product['price'] . '$</div>
-                            <div class="remove">
-                                <button>&times;</button>
-                            </div>
-                        </div>';
-        }
+    if ($product = $result->fetch_assoc()) {
+        $html .= '<div class="item">
+                        <div class="image">
+                            <img src="shoes_imgs/'.$product['imageURL'].'">
+                        </div>
+                        <div class="name">' . $product['product_name'] . '</div>
+                        <div class="totalPrice">' . $product['price'] . '$</div>
+                        <div class="remove">
+                            <button>&times;</button>
+                        </div>
+                    </div>';
     }
-    echo $html;
+    return $html;
 }
-?>
