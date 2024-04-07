7e06609f58ee4c5930886abcfb8197c40ced218c
Ibrahim Dayraki <i.dayraki@gmail.com>
2024-04-07 10:40:58 +0300

    admin styling

--- admin/view.php
+++ admin/view.php
@@ -14,15 +14,17 @@ $resultproduct = $resultproduct->fetch_assoc()
 <html>
 
 <head>
-    <link href="/resource/css/admin.css" rel="stylesheet">
+    <link href="admin.css" rel="stylesheet">
     <title>View Shoe</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 
 <body>
-    <a href="index.php">Back</a>
+    <a href="index.php" class="btn btn-primary mb-3" >Back</a>
+    <h2 class="text-center mb-5">View Product </h2>
     <div>
-        <img src="../shoes_imgs/<?php echo $resultproduct['imageURL'] ?>" alt="" />
+        <img class="mb-4" style="width: 30%;" src="../shoes_imgs/<?php echo $resultproduct['imageURL'] ?>" alt="" />
         <div>ID: <?php echo $id ?></div>
         <div>Name: <?php echo $resultproduct['product_name'] ?></div>
         <div>Price: <?php echo $resultproduct['price'] ?></div>
